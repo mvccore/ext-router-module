9adffaa3a8304faf16787485ce0524bda39a519a
Tom Flídr <tomflidr@gmail.com>
2024-11-21 16:01:01 +0100

    release v5.3

--- src/MvcCore/Ext/Routers/Module.php
+++ src/MvcCore/Ext/Routers/Module.php
@@ -48,7 +48,7 @@ implements	\MvcCore\Ext\Routers\IModule,
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.2.0';
+	const VERSION = '5.3.0';
 
 }
 
